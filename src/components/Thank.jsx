ccc73dabd5da67951adb6690acc1cc7de8a7dc48
Chonthichar <chonthichar.soythong@gmail.com>
2022-05-30 16:05:21 +0700

    styling for window page

--- src/components/Thank.jsx
+++ src/components/Thank.jsx
@@ -5,7 +5,7 @@ function Thank() {
                 <p>😊</p>
                 <h5>Thank you for your submission. <br/> We will get back to you shortly 🍻</h5>
                 <br/>
-                <br/> <div className='home-page'><a href="https://www.makeweb.info/thank/">Go back to the Home Page</a></div>
+                <br/> <div className='home-page'><a href="https://www.makeweb.info/">Go back to the Home Page</a></div>
             </div>
         </>
     )
